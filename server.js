11864f838ab04014f5b45931f22bd62f2fec7658
webcrunch <jarllinduist@gmail.com>
2023-04-17 15:13:14 +0200

    test github actions with cypress

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5500
 const path = require('path');
 
 app.use(express.static('luma-shop'))
-
-/*app.get('/', (req, res) => {
-    console.log(path.join(__dirname, 'index.html'));
-  res.sendFile(path.join(__dirname, 'index.html'))
-})*/
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
